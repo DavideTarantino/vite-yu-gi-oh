f8e59c604174aafa0aedd16ee746cfaee82f28ed
DavideTarantino <davidetarantino2005@gmail.com>
2024-02-10 21:13:18 +0100

    inserimento dello store

--- format-vue/src/App.vue
+++ format-vue/src/App.vue
@@ -1,18 +1,44 @@
 <script>
 import HeaderBar from './components/header/HeaderBar.vue';
-import AppSearch from './components/main/AppSearch.vue' 
+import AppSearch from './components/main/AppSearch.vue';
+import CardList from './components/main/CardList.vue'; 
+
+import{ store } from './store'
+import axios from 'axios'
 
 export default{
     components:{
         HeaderBar,
-        AppSearch
+        AppSearch,
+        CardList
+    },
+    data(){
+        return{
+            store
+        }
+    },
+    methods: {
+        getCards(){
+            axios.get( store.apiUrl )
+            .then( res => {
+                console.log(res.data)
+
+                store.cardList = res.data.data
+            } )
+        }
+    },
+    mounted() {
+        this.getCards()
     }
 }
 </script>
 
 <template>
     <HeaderBar/>
-    <AppSearch/>
+    <main>
+        <AppSearch/>
+        <CardList/>
+    </main>
 </template>
 
 <style lang="scss">
